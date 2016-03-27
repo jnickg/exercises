3aa08ffec61a7d9891f4f779743ce88f1c1c901d
jnickg <jnickg83@gmail.com>
2016-03-26 18:15:57 -0700

    Remove bad include & add commenting

--- src/file_1.h
+++ src/file_1.h
@@ -1,5 +1,8 @@
-#include "file_1.h"
+// This file includes some very useful functions.
+// It may also have some that are less useful.
 
+// Foos the bar
 float foo(int bar);
 
+// Boos the far by adding the far with the fooed far.
 float boo(float far);
